5e2d023253ef7ea5ced0de4bb6c2d0f066fbaf37
陶智威 <zowie.tao@bytedance.com>
2023-12-28 14:01:04 +0800

    chore: style change

--- app/scope_counter/layout.tsx
+++ app/scope_counter/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">Scope counter layout:
+    <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left text-4xl font-extrabold">🔱 counter layout:
       {children}
     </div>
   )
