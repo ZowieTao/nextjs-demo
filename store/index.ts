47d2da4c2433910b1d22bccf59fd77dfaade697f
陶智威 <zowie.tao@bytedance.com>
2024-01-03 01:07:16 +0800

    feat: devtools debugger

--- store/index.ts
+++ store/index.ts
@@ -1,13 +1,14 @@
 import { StateCreator, StoreMutatorIdentifier, create } from 'zustand'
 import { IBearSlice, createBearSlice } from './bearSlice'
 import { IFishSlice, createFishSlice } from './fishSlice'
+import { devtools } from 'zustand/middleware'
 
 type IBoundStore = IBearSlice & IFishSlice
 
 export type BoundStoreSlice<T, Mis extends [StoreMutatorIdentifier, unknown][] = [], Mos extends [StoreMutatorIdentifier, unknown][] = []> = StateCreator<IBoundStore, Mis, Mos, T>
 
 
-export const useBoundStore = create<IBoundStore>()((set, get, store) => ({
+export const useBoundStore = create<IBoundStore>()(devtools((set, get, store) => ({
   ...createBearSlice(set, get, store),
   ...createFishSlice(set, get, store),
-}))
+}), { enabled: process.env.NODE_ENV !== 'production', name: 'Bound Store' }))
