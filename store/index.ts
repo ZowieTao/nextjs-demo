eba785944ff6c008a0e937911c55592998615a40
陶智威 <zowie.tao@bytedance.com>
2024-01-02 09:13:19 +0800

    chore: typescript curried

--- store/index.ts
+++ store/index.ts
@@ -7,7 +7,7 @@ type IBoundStore = IBearSlice & IFishSlice
 export type BoundStoreSlice<T, Mis extends [StoreMutatorIdentifier, unknown][] = [], Mos extends [StoreMutatorIdentifier, unknown][] = []> = StateCreator<IBoundStore, Mis, Mos, T>
 
 
-export const useBoundStore = create<IBoundStore>((set, get, store) => ({
+export const useBoundStore = create<IBoundStore>()((set, get, store) => ({
   ...createBearSlice(set, get, store),
   ...createFishSlice(set, get, store),
 }))
