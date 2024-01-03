d4391e6cb3ed1e00a7f2a4fc91f2f0fb49f8835c
陶智威 <zowie.tao@bytedance.com>
2024-01-03 09:43:29 +0800

    feat: subscribe and subscribe wit h selector

--- app/fish_counter/page.tsx
+++ app/fish_counter/page.tsx
@@ -2,6 +2,8 @@
 
 import { useBoundStore } from "@/store"
 import Link from "next/link"
+import { memo, useEffect, useState } from "react"
+import { shallow } from "zustand/shallow"
 
 export default function LoginPage() {
 
@@ -46,6 +48,74 @@ export default function LoginPage() {
           Click here will plus global fish state, let&apos;s check out: {bear}
         </p>
       </Link>
+
+      <FishTip />
+
     </>
   )
+}
+
+
+
+const FishTip = memo(_FishTip)
+
+
+function _FishTip() {
+  console.log('==fish tip render==')
+
+  const addBear = useBoundStore((state) => {
+    return state.addBear
+  })
+
+  const [text, setText] = useState('')
+  // const [text, setText] = useState(useBoundStore.getState().fishes > 25 ? 'Too many fishes' : 'Small amount of fish')
+
+  useEffect(() => {
+    /*     const cancelSub = useBoundStore.subscribe((state, prevState) => {
+          console.log('==fish tip subscribe==', state.fishes, prevState.fishes)
+    
+          if (state.fishes > 25) {
+            setText('Too many fishes')
+          } else if (state.fishes <= 25) {
+            setText('Small amount of fish')
+          }
+        })
+    
+        return cancelSub */
+
+    const cancelSub = useBoundStore.subscribe(
+      state => state.fishes,
+      (fishes, prevFishes) => {
+        console.log('==fish tip subscribeWithSelector==', fishes, prevFishes)
+
+        if (fishes > 25) {
+          setText('Too many fishes')
+        } else if (fishes <= 25) {
+          setText('Small amount of fish')
+        }
+      },
+      { equalityFn: shallow, fireImmediately: true }
+    )
+
+    return cancelSub
+
+  }, [])
+
+
+
+  return <a
+    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 select-none"
+    rel="noopener noreferrer"
+    onClick={addBear}
+  >
+    <h2 className={`mb-3 text-2xl font-semibold`}>
+      Fish statistic
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
+    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+      {text} {`(click to add bear)`}
+    </p>
+  </a>
 }
--- store/index.ts
+++ store/index.ts
@@ -2,13 +2,16 @@ import { StateCreator, StoreMutatorIdentifier, create } from 'zustand'
 import { IBearSlice, createBearSlice } from './bearSlice'
 import { IFishSlice, createFishSlice } from './fishSlice'
 import { devtools } from 'zustand/middleware'
+import { subscribeWithSelector } from 'zustand/middleware'
 
 type IBoundStore = IBearSlice & IFishSlice
 
 export type BoundStoreSlice<T, Mis extends [StoreMutatorIdentifier, unknown][] = [], Mos extends [StoreMutatorIdentifier, unknown][] = []> = StateCreator<IBoundStore, Mis, Mos, T>
 
 
-export const useBoundStore = create<IBoundStore>()(devtools((set, get, store) => ({
+// immer(devtools(subscribeWithSelector(persist(()=>{})))) 
+
+export const useBoundStore = create<IBoundStore>()(devtools(subscribeWithSelector((set, get, store) => ({
   ...createBearSlice(set, get, store),
   ...createFishSlice(set, get, store),
-}), { enabled: process.env.NODE_ENV !== 'production', name: 'Bound Store' }))
+})), { enabled: process.env.NODE_ENV !== 'production', name: 'Bound Store' }))
